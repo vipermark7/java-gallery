0dfc401604261e17b5c41ecf0795c50e6060f7f1
Shaffan <shaffan1996@gmail.com>
2024-03-07 02:46:06 -0500

    tried to get home page to work. could now :/

--- demo/src/main/java/com/example/demo/controllers/PictureController.java
+++ demo/src/main/java/com/example/demo/controllers/PictureController.java
@@ -6,15 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/pictures")
 public class PictureController {
-
     @Autowired
     private PictureService pictureService;
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
 
     @GetMapping
     public List<Picture> getAllPictures() {
